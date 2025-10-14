bdc2588512903c08adb1addeb808cefa0ba6ff22
CubeAndy <a072682@gmail.com>
2025-10-14 13:05:11 +0800

    小修改

--- src/slice/orderSlice.js
+++ src/slice/orderSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";//一開始先import createSlice(系統通常會自動抓取)
-import { axiosWithCookie, BASE_URL } from "../../api";
+import { axiosWithCookie, BASE_URL } from "../api";
 
 
 //此區塊為測試開發用內容
